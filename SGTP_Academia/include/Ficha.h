bc43fbfecd0094b0525a7d6e916dec66f96408e0
Talisson Pereira de Oliveira <89871517+TalissonOliveira14@users.noreply.github.com>
2025-12-06 22:34:52 -0300

    Create Ficha.h

---
+++ SGTP_Academia/include/Ficha.h
@@ -0,0 +1,37 @@
+#ifndef FICHA_H
+#define FICHA_H
+
+#include <vector>
+#include <string>
+#include "Exercicio.h"
+
+class Ficha {
+private:
+    int id;
+    std::string nome;
+    std::vector<Exercicio*> exercicios;
+
+public:
+    Ficha();
+    Ficha(int id, std::string nome);
+    ~Ficha();
+    
+    int getId() const;
+    std::string getNome() const;
+    void setNome(std::string nome);
+    
+    void adicionarExercicio(Exercicio* exercicio);
+    void removerExercicio(int index);
+    void limparExercicios();
+    
+    double calcularTempoTotal() const;
+    double calcularCaloriasTotais() const;
+    void exibirDetalhes() const;
+    
+    std::string toFileString() const;
+    static Ficha fromFileString(std::string linha, std::vector<Exercicio*> listaExercicios);
+    
+    std::vector<Exercicio*> getExercicios() const;
+};
+
+#endif
