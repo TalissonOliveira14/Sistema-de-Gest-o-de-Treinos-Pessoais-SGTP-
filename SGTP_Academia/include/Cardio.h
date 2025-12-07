d75f52371f014d9263da186ffe0e5d18d18d42f0
Talisson Pereira de Oliveira <89871517+TalissonOliveira14@users.noreply.github.com>
2025-12-06 22:31:17 -0300

    Add Cardio class definition in Cardio.h

---
+++ SGTP_Academia/include/Cardio.h
@@ -0,0 +1,25 @@
+#ifndef CARDIO_H
+#define CARDIO_H
+
+#include "Exercicio.h"
+
+class Cardio : public Exercicio {
+private:
+    int duracao;
+    double caloriasPorMinuto;
+
+public:
+    Cardio();
+    Cardio(std::string nome, int duracao, double caloriasPorMinuto);
+    Cardio(int idExistente, std::string nome, bool status, int duracao, double caloriasPorMinuto);
+    
+    double calcularTempo() const override;
+    double calcularCalorias() const override;
+    void exibirDetalhes() const override;
+    std::string toFileString() const override;
+    
+    int getDuracao() const;
+    double getCaloriasPorMinuto() const;
+};
+
+#endif
