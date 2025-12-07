b557865ada6a0e2e6b467004429836dd8b3c195c
Talisson Pereira de Oliveira <89871517+TalissonOliveira14@users.noreply.github.com>
2025-12-07 13:26:26 -0300

    Update Utils.cpp

--- SGTP_Academia/src/Utils.cpp
+++ SGTP_Academia/src/Utils.cpp
@@ -20,17 +20,6 @@ std::string Utils::getDataAtual() {
     return std::string(buffer);
 }
 
-std::string Utils::formatarTempo(double minutos) {
-    int horas = minutos / 60;
-    int mins = (int)minutos % 60;
-    int segs = (int)((minutos - (int)minutos) * 60);
-    
-    if (horas > 0) {
-        return std::to_string(horas) + "h " + std::to_string(mins) + "min";
-    }
-    return std::to_string(mins) + "min " + std::to_string(segs) + "s";
-}
-
 double Utils::lerDouble(const std::string& mensagem) {
     double valor;
     while (true) {
@@ -53,7 +42,7 @@ int Utils::lerInt(const std::string& mensagem) {
             std::cin.ignore();
             return valor;
         }
-        std::cout << "Valor invalido. Trente novamente.\n";
+        std::cout << "Valor invalido. Tente novamente.\n";
         std::cin.clear();
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
@@ -64,11 +53,4 @@ std::string Utils::lerString(const std::string& mensagem) {
     std::cout << mensagem;
     std::getline(std::cin, valor);
     return valor;
-}
-
-bool Utils::confirmar(const std::string& mensagem) {
-    std::cout << mensagem << " (S/N): ";
-    std::string resposta;
-    std::getline(std::cin, resposta);
-    return !resposta.empty() && (resposta[0] == 'S' || resposta[0] == 's');
 }
